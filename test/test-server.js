19d05bc7784239357160b9017f53bfba18314ac7
haydenmcp <hayden.mcparlane@gmail.com>
2018-02-25 14:25:24 -0600

    updated describe name

--- test/test-server.js
+++ test/test-server.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const request = require('request');
 const { host } = require('../configuration.json');
 
-describe('server.js', () => {
+describe('graphql services', () => {
   // TODO Modify tests to actually cover required cases.
   it('should allow users to hit the graphql end point', (done) => {
     request(`http://${host.ip}:${host.port}/graphql?query={hello}`, (error, response, body) => {
